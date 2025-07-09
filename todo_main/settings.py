6200af9d734777dfc8e6b3b7e6c768d8169db735
Sourabh101 <sengersourabh101@gmail.com>
2025-07-09 17:55:40 +0530

    Fix configs remove ignore

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config, RepositoryIni
+from decouple import config, RepositoryIni
 
 
 
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Config('DEBUG' , default=False, cast=bool) # type: ignore
+DEBUG = config('DEBUG' , default=False, cast=bool) 
 
 ALLOWED_HOSTS = ['.onrender.com']
 
