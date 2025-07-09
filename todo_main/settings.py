21a28642f95b1ee5aec54c6855795bce805498c6
Sourabh101 <sengersourabh101@gmail.com>
2025-07-09 17:49:54 +0530

    Fix config

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = Config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG' , default=False, cast=bool)
+DEBUG = Config('DEBUG' , default=False, cast=bool)
 
 ALLOWED_HOSTS = ['.onrender.com']
 
